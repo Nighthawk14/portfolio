3e7f042dcdd5cb3437c2c6fc886a2aa27650604e
NightHawk14 <antoine.savignac@gmail.com>
2014-07-16 18:32:51 +0200

    initial commit v2

--- README.md
+++
@@ -1,4 +0,0 @@
-portfolio_2013
-==============
-
-Portfolio Antoine Savignac 2013
---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name antoinesavignacfrApp
+ * @description
+ * # antoinesavignacfrApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('antoinesavignacfrApp', []);

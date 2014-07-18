aa006a9ad5e2013d457239086a431077fa23a17b
NightHawk14 <antoine.savignac@gmail.com>
2014-07-18 12:02:53 +0200

    update projects, flip + organisation + size

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -84,5 +84,111 @@ angular.module('antoinesavignacfrApp')
         link: 'http://ipassemploi.com/',
         _class:'ipassemploi'
       },
+      {
+        title: 'City Dashboard',
+        description: 'NodeJs - AngularJs',
+        link: 'http://www.city-dashboard.com',
+        _class:'city-dashboard'
+      },
+      {
+        title: 'Kariboo',
+        description: 'Objective C - AngularJs',
+        link: '#',
+        event: {active:true,value:'kariboo'},
+        _class:'kariboo'
+      },
+      {
+        title: 'Les Feux verts',
+        description: 'IronWeb - FrontEnd - JSON',
+        link: 'http://www.lesfeuxverts.com',
+        event: {active:true,value:'feux-verts'},
+        _class:'feux-vert'
+      },
+      {
+        title: 'Palm',
+        description:'Php - Google API - D3JS',
+        link: 'http://www.palm-analytics.com',
+        _class:'palm'
+      },
+      /*{
+        title: 'Benoit Dartigues',
+        description: 'PHP - MySQL',
+        link: 'http://www.benoitdartigues.fr/',
+        _class:'benoit-bg.png'
+      },*/
+      {
+        title: 'Semaine-digitale',
+        description: 'ChartJs - RaphaelJs - CSS',
+        link: 'http://villedebordeaux.fr/dataviz',
+        _class:'semaine-digitale'
+      },
+      {
+        title: 'Daubert-Panasyuk',
+        description: 'Wordpress - AngularJs',
+        link: 'http://daubert-panasyuk.com/',
+        _class:'daubert'
+      },
+      {
+        title: 'Ipass Emploi',
+        description: 'CodeIgniter - Réseau social',
+        link: 'http://ipassemploi.com/',
+        _class:'ipassemploi'
+      },
+      {
+        title: 'Ipass Emploi',
+        description: 'CodeIgniter - Réseau social',
+        link: 'http://ipassemploi.com/',
+        _class:'ipassemploi'
+      },
+      {
+        title: 'City Dashboard',
+        description: 'NodeJs - AngularJs',
+        link: 'http://www.city-dashboard.com',
+        _class:'city-dashboard'
+      },
+      {
+        title: 'Kariboo',
+        description: 'Objective C - AngularJs',
+        link: '#',
+        event: {active:true,value:'kariboo'},
+        _class:'kariboo'
+      },
+      {
+        title: 'Les Feux verts',
+        description: 'IronWeb - FrontEnd - JSON',
+        link: 'http://www.lesfeuxverts.com',
+        event: {active:true,value:'feux-verts'},
+        _class:'feux-vert'
+      },
+      {
+        title: 'Palm',
+        description:'Php - Google API - D3JS',
+        link: 'http://www.palm-analytics.com',
+        _class:'palm'
+      },
+      /*{
+        title: 'Benoit Dartigues',
+        description: 'PHP - MySQL',
+        link: 'http://www.benoitdartigues.fr/',
+        _class:'benoit-bg.png'
+      },*/
+      {
+        title: 'Semaine-digitale',
+        description: 'ChartJs - RaphaelJs - CSS',
+        link: 'http://villedebordeaux.fr/dataviz',
+        _class:'semaine-digitale'
+      },
+      {
+        title: 'Daubert-Panasyuk',
+        description: 'Wordpress - AngularJs',
+        link: 'http://daubert-panasyuk.com/',
+        _class:'daubert'
+      },
+      {
+        title: 'Ipass Emploi',
+        description: 'CodeIgniter - Réseau social',
+        link: 'http://ipassemploi.com/',
+        _class:'ipassemploi'
+      },
     ];
   }]);
--- app/scripts/directives/project.js
+++ app/scripts/directives/project.js
@@ -7,9 +7,40 @@
  * # Project
  */
 angular.module('antoinesavignacfrApp')
-  .directive('project', function () {
+  .directive('project',[function () {
+    var base = 6;
+    var _index = 0;
+    var _row = 0;
+
+    var getOffset = function(){return -27*_row;};
     return {
       templateUrl: 'partials/project.html',
-      restrict: 'EA'
+      restrict: 'EA',
+      link: function($scope, iElm) {
+        var i = $scope.$index;
+        
+        if(_index % base === 0 && _index !== 0)
+        {
+          base = (base === 6) ? 5 : 6;
+          _index = 0;
+          $(iElm).before('<div class="clear"></div>');
+          _row++;
+        }
+        var _class = (base === 6) ? 'even' : 'odd';
+        $(iElm).addClass(_class);
+
+        if(i >= 6){
+          $(iElm).css('top',getOffset());
+        }
+        _index++;
+
+        
+        if($scope.$last){
+          var $w = $('#projects').find('.wrapper');
+          $w.height($w.height()+(getOffset()));
+          $('#projects').height(($(iElm).offset().top+$(iElm).height())-($('#projects').offset().top));
+        }
+        
+      }
     };
-  });
+  }]);

713b2d9663fd41703a8303236c519f867a568ed5
antoine.savignac <antoine.savignac@gmail.com>
2015-12-20 13:30:17 -0500

    Add projects data

--- src/app/projects/projects.ts
+++ src/app/projects/projects.ts
@@ -1,6 +1,7 @@
 import {Injectable, Component} from 'angular2/core';
 import {Project} from './project';
 import {TranslatePipe} from 'ng2-translate/ng2-translate';
+var projects = require('./projectsData.ts');
 
 @Component({
   selector: 'projects',
@@ -12,5 +13,10 @@ export class Projects {
   projects: Array<Project>;
   addProject(project: Project) {
     this.projects.push(project);
+  },
+  ngOnInit() {
+    projects.map(project => {
+      this.addProject(new Project(project));
+    })
   }
 }

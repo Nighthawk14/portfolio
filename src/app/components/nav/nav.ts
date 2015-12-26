a8521084118f69fb90ad523058d65aa8a8c19ce9
antoine.savignac <antoine.savignac@gmail.com>
2015-12-26 04:46:24 -0500

    Fix nav component. Need to add jquery logic. Style ok. Need to add translation logic

--- src/app/components/nav/nav.ts
+++ src/app/components/nav/nav.ts
@@ -1,8 +1,68 @@
 import {Component} from 'angular2/core';
 
 @Component({
-  selector: 'nav',
+  selector: '[nav]',
   template: require('./nav.html'),
-  styles: [require('./styles/nav/_nav.scss')]
+  styles: [require('../../styles/nav/_nav.scss')]
 })
-export class Nav {}
+export class Nav {
+  moveTo(tag) {
+    let {elem, offset} = this.getOffset(tag);
+//     $document.scrollTo(elem, offset,800).then(function(){
+//     $location.hash(tag);
+//   });
+  }
+  
+  ngOnInit() {
+//     $(element).css('top',$(window).height()/2 - $(element).height()/2);
+  }
+  
+  private getOffset(tag) {
+    let elem;
+    let offset;
+    switch(tag)
+    {
+      case 'techs':
+        elem = '#technos';
+        offset = -30;
+      break;
+
+      case 'projects':
+        elem = '#timeline';
+        offset = 30;
+      break;
+
+      default:
+        elem = '#home';
+        offset = 0;
+      break;
+    }
+    
+    return {elem, offset};
+  }
+}
+
+// var setTranslation = function(){
+//   $(element).find('.nav-link').each(function(k,value){
+//     var key = $(value).attr('data-key');
+//     $translate(key).then(function (translation) {
+//       $(value).attr({
+//         'data-original-title':translation,
+//         'title':translation
+//       });
+//     });
+//   });
+// };
+// scope.$parent.$parent.$watch('lang', function(){
+//   setTranslation();
+// });
+// $(element).find('.nav-link').tooltip();
+// $(element).css('top',$(window).height()/2 - $(element).height()/2);
+// $('nav a').on('click', function(e){
+//   e.preventDefault();
+//   var offset,elem;
+//   var tag = $(this).attr('data-element');
+//   $document.scrollTo(elem, offset,800).then(function(){
+//     $location.hash(tag);
+//   });
+// });

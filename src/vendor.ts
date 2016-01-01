b79f75eb554b372c15d049adeb314b502000a65c
antoine.savignac <antoine.savignac@gmail.com>
2016-01-01 12:33:31 -0500

    Remove router/http from app dependencies

--- src/vendor.ts
+++ src/vendor.ts
@@ -10,8 +10,8 @@ import 'zone.js/lib/browser/zone-microtask';
 import 'angular2/platform/browser';
 import 'angular2/platform/common_dom';
 import 'angular2/core';
-import 'angular2/router';
-import 'angular2/http';
+//import 'angular2/router';
+//import 'angular2/http';
 
 // RxJS
 import 'rxjs';
